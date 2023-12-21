2b2cb4b7c7148c6f0dd2410746dbf44895c64e6a
vtonu <victor.tonu@yahoo.com>
2023-12-20 22:45:52 -0800

    Removed unused imports in index.js

--- src/index.js
+++ src/index.js
@@ -2,9 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
-import { theme } from './theme';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
